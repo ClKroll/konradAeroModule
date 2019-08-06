Corrected scaling: only multiply ext not scat, albedo in aerosol.py

konrad/aerosol.py
@@ -85,7 +85,7 @@ class VolcanoAerosol(Aerosol):
                         extEarth.ext_earth[8,lw_band, :].values,
                         #extEarth.ext_earth[lw_band, :, 1].values,
                         bounds_error=False,
-                        fill_value=0)(heights)/scaling
+                        fill_value=0)(heights)*scaling
                     
 
             for sw_band in range(np.shape(extSun.solar_bands)[0]):
@@ -95,21 +95,21 @@ class VolcanoAerosol(Aerosol):
                         extSun.ext_sun[sw_band,8, :].values,
                         #extSun.ext_sun[sw_band, :, 1],
                         bounds_error=False,
-                        fill_value=0)(heights)/scaling
+                        fill_value=0)(heights)*scaling
                 self.asymmetry_factor_aerosol_sw[sw_band, :] = \
                     sc.interpolate.interp1d(
                         gSun.altitude.values,
                         gSun.g_sun[sw_band,8, :].values,
                         #gSun.g_sun[sw_band, :, 1].values,
                         bounds_error=False,
-                        fill_value=0)(heights)/scaling
+                        fill_value=0)(heights)
                 self.single_scattering_albedo_aerosol_sw[sw_band, :] = \
                     sc.interpolate.interp1d(
                         omegaSun.altitude.values,
                         omegaSun.omega_sun[sw_band,8, :].values,
                         #omegaSun.omega_sun[sw_band, :, 1].values,
                         bounds_error=False,
-                        fill_value=0)(heights)/scaling
+                        fill_value=0)(heights)
 
     def calculateHeightLevels(self, atmosphere):
         heights = ty.pressure2height(atmosphere['plev'], atmosphere['T'][0, :])/1000