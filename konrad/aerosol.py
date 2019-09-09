2bbb3372ea50aef8a90e1cee8823a9e611f0e162
ClKroll <clarissa.kroll@t-online.de>
2019-09-09 11:24:49 +0200

    Added documentation of aerosol class

--- konrad/aerosol.py
+++ konrad/aerosol.py
@@ -1,3 +1,62 @@
+"""This module integrates volcanic aerosols into konrad based on observational data
+(CMIP6 dataset (e.g. Thomason et al., 2018), Pinatubo eruption, loading between 23 S 
+and 23 N). They can be used either in the RCE simulations or simply for radiative 
+flux or heating rate calculations.
+The default setting is excluding volcanic aerosols.
+
+**In an RCE simulation**
+
+Create an instance of a aerosol class,  
+create an appropriate radiation model, and run an RCE simulation.
+    >>> import konrad
+    >>> aerosol = konrad.aerosol.VolcanoAerosol(
+    >>>     atmNumlevels=numlevels, aerosolLevelShiftInput=...,includeSWForcing=..,
+    >>>     includeLWForcing=...,includeScattering=...,includeAbsorption=...,
+    >>>     )
+    >>> rce = konrad.RCE(atmosphere=..., aerosol=aerosol)
+    >>> rce.run()
+
+**Calculating radiative fluxes or heating rates**
+
+Create an instance of a cloud class, *e.g.* a :py:class:`PhysicalCloud`,
+create an appropriate radiation model and run radiative transfer.
+    >>> import konrad
+    >>> numlevels=201
+    >>> plev, phlev = konrad.utils.get_pressure_grids(1000e2, 1,numlevels)
+    >>> atmosphere = konrad.atmosphere.Atmosphere(plev)
+    >>> aerosol = konrad.aerosol.VolcanoAerosol(
+    >>>     atmNumlevels=numlevels, aerosolLevelShiftInput=...,includeSWForcing=..,
+    >>>     includeLWForcing=...,includeScattering=...,includeAbsorption=...,
+    >>>     )
+    >>> rrtmg.calc_radiation(atmosphere, surface=...,volcanoAerosol) 
+    
+The aerosol class has a set of parameters:
+    atmNumLevels: the number of pressure levels set for the atmosphere, also 
+                    referred to as numlevels
+    aerosol_type: choose between 'no_aerosol' for no volcanic aerosols
+                                 'all_aerosol_properties' for volcanic aerosols,
+                                      described by LW (ext) and SW (ext, g, ssa)
+                                  'xxx' for volcanic aerosols, described by LW
+                                      (ext) and SW (ext at 550 mu m) (currently
+                                      not implemented in version available online)
+    aerosolLevelShiftInput: choose the shift of the aerosol layer relative to the
+                    original Pinatubo aerosol layer (units in km)
+    monthsAfterEruption: chose the forcing data to describe the aerosol characteristics
+                         x months after the eruption. Mount Pinatubo erupted on June 15, 1991.
+                         allowed inputs in [0,18]
+                         the input starts at 1 (July 1991).
+                         Default value 2 is August 1991.
+                         xxx not implemented yet xxxx 
+    includeSWForcing: if True: imports the SW forcing files and calculates the radiative
+                         fluxes using them
+    includeScattering: includes SW scattering component
+                        (only for 'all_aerosol_properties')
+    includeScattering: includes SW absorption component
+                        (only for 'all_aerosol_properties')
+    includeLWForcing: if True: imports the LW forcing files and calculates the radiative
+                         fluxes using them
+"""
+ 
 import os
 import abc
 import xarray as xr
@@ -13,7 +72,7 @@ from konrad.cloud import get_waveband_data_array
 
 
 class Aerosol(metaclass=abc.ABCMeta):
-    def __init__(self,atmNumlevels, aerosol_type='no_aerosol', aerosolLevelShiftInput=0,includeSWForcing=True,includeLWForcing=True,includeScattering=True,includeAbsorption=True):
+    def __init__(self,atmNumlevels, aerosol_type='no_aerosol', aerosolLevelShiftInput=0,monthsAfterEruption=2,includeSWForcing=True,includeLWForcing=True,includeScattering=True,includeAbsorption=True):
          
         a = get_waveband_data_array(0, units='dimensionless', numlevels=atmNumlevels, sw=True)   #called ext_sun in files
         b = get_waveband_data_array(0, units='dimensionless', numlevels=atmNumlevels, sw=True)    #called omega_sun in files
@@ -33,7 +92,6 @@ class Aerosol(metaclass=abc.ABCMeta):
     #################################################################
     #To do: time step updating
     #For now the aerosols are left constant and are not updated
-    #add numlevels to init
     #implementation for a changing lapse rate, for now it is implemented only for a fixed lapse rate
     ################################################################
     def update_aerosols(self, time, atmosphere):
@@ -44,37 +102,51 @@ class Aerosol(metaclass=abc.ABCMeta):
 
 
 class VolcanoAerosol(Aerosol):
-    def __init__(self,atmNumlevels, aerosolLevelShiftInput=0):
+    '''
+    CMIP6 volcanic aerosols (Mount Pinatubo eruption)
+    '''
+    def __init__(self,atmNumlevels, aerosolLevelShiftInput=0,includeSWForcing=True,includeLWForcing=True,includeScattering=True,includeAbsorption=True):
         super().__init__(atmNumlevels,aerosol_type='all_aerosol_properties')
         self.aerosolLevelShift=aerosolLevelShiftInput
+        self.numlevels=atmNumlevels
+        self.includeSWForcing=includeSWForcing
+        self.includeLWForcing=includeLWForcing
+        self.includeScattering=includeScattering
+        self.includeAbsorption=includeAbsorption
 
     def update_aerosols(self, time, atmosphere):
+        '''
+        Import the volcanic forcing data in first time step, taking into account
+        the parameter settings of the VolcanoAerosol class.
+        Translate from height dependance to pressure dependance.
+        The aerosol layer is kept fixed and constant throughout the following run.
+        '''
         if not np.count_nonzero(self.optical_thickness_due_to_aerosol_sw.values):
             if self.includeLWForcing:
                 extEarth = xr.open_dataset(
                         os.path.join(
                                 os.path.dirname(__file__),
                                 'data/aerosolData/23dataextEarth1991.nc'
-                                #'data/aerosolData/zonAverageExtEarthbc_aeropt_cmip6_volc_lw_b16_sw_b14_1992.nc'
+                                #'data/aerosolData/23dataextEarth1992.nc'
                                 ))
             if self.includeSWForcing:
                 extSun = xr.open_dataset(
                         os.path.join(
                                 os.path.dirname(__file__),
                                 'data/aerosolData/23dataextSun1991.nc'
-                                #'data/aerosolData/zonAverageExtSunbc_aeropt_cmip6_volc_lw_b16_sw_b14_1992.nc'
+                                #'data/aerosolData/23dataextSun1992.nc'
                                 ))
                 gSun = xr.open_dataset(
                         os.path.join(
                                 os.path.dirname(__file__),
                                 'data/aerosolData/23datagSun1991.nc'
-                                #'data/aerosolData/zonAveragegSunbc_aeropt_cmip6_volc_lw_b16_sw_b14_1992.nc'
+                                #'data/aerosolData/23datagSun1992.nc'
                                 ))
                 omegaSun = xr.open_dataset(
                         os.path.join(
                                 os.path.dirname(__file__),
                                 'data/aerosolData/23dataomegaSun1991.nc'
-                                #'data/aerosolData/zonAverageOmegaSunbc_aeropt_cmip6_volc_lw_b16_sw_b14_1992.nc'
+                                #'data/aerosolData/23dataomegaSun1992.nc'
                                 ))
             heights = self.calculateHeightLevels(atmosphere)
             #the input data has to be scaled to fit to model levels
@@ -82,10 +154,12 @@ class VolcanoAerosol(Aerosol):
             scaling=np.gradient(heights)
             
             if self.aerosolLevelShift:
-                self.aerosolLevelShiftArray=self.aerosolLevelShift*np.ones(np.shape(extEarth.altitude[:]))
+ 
                 if self.includeLWForcing:
+                    self.aerosolLevelShiftArray=self.aerosolLevelShift*np.ones(np.shape(extEarth.altitude[:]))
                     extEarth.altitude.values=extEarth.altitude.values+self.aerosolLevelShiftArray
                 if self.includeSWForcing:
+                    self.aerosolLevelShiftArray=self.aerosolLevelShift*np.ones(np.shape(extSun.altitude[:]))
                     extSun.altitude.values=extSun.altitude.values+self.aerosolLevelShiftArray
                     gSun.altitude.values=gSun.altitude.values+self.aerosolLevelShiftArray
                     omegaSun.altitude.values=omegaSun.altitude.values+self.aerosolLevelShiftArray
@@ -123,26 +197,31 @@ class VolcanoAerosol(Aerosol):
                             #omegaSun.omega_sun[sw_band, :, 1].values,
                             bounds_error=False,
                             fill_value=0)(heights)
-            
-                if not self.includeScattering: '''only absorption'''
+              # '''only absorption'''
+                if not self.includeScattering: 
                     try:
-                        self.optical_thickness_due_to_aerosol_sw= \
-                                    np.multiply(self.optical_thickness_due_to_aerosol_sw,
-                                                np.subtract(np.ones_like(self.optical_thickness_due_to_aerosol_sw),
-                                                            self.single_scattering_albedo_aerosol_sw))
-                        self.asymmetry_factor_aerosol_sw=np.zeros_like(self.asymmetry_factor_aerosol_sw)
-                        self.single_scattering_albedo_aerosol_sw=np.zeros_like(self.single_scattering_albedo_aerosol_sw)
+                        a=get_waveband_data_array(1, units='dimensionless', numlevels=self.numlevels, sw=True).T
+                        result= np.multiply(self.optical_thickness_due_to_aerosol_sw, np.subtract(a,self.single_scattering_albedo_aerosol_sw)),
+                                            
+                        self.optical_thickness_due_to_aerosol_sw=get_waveband_data_array(result[0].values.T, units='dimensionless', numlevels=self.numlevels, sw=True).T
+                                    #__sub__(self, other)
+
+                        #self.asymmetry_factor_aerosol_sw= (get_waveband_data_array(0, units='dimensionless', numlevels=self.numlevels, sw=True)).T 
+                       
+                        self.single_scattering_albedo_aerosol_sw = get_waveband_data_array(0, units='dimensionless', numlevels=self.numlevels, sw=True).T 
                         if not self.includeAbsorption:
                             raise ValueError('For aerosols scattering and absorption can not both be deactivated')
                     except (ValueError):
                         exit('Please choose valid input data.')
                         
-                if not self.includeAbsorption: '''only scattering'''
+                if not self.includeAbsorption: #'''only scattering'''
                     try:
-                        self.optical_thickness_due_to_aerosol_sw= \
+                        result= \
                                     np.multiply(self.optical_thickness_due_to_aerosol_sw,
                                                 self.single_scattering_albedo_aerosol_sw)
-                        self.single_scattering_albedo_aerosol_sw=np.ones_like(self.single_scattering_albedo_aerosol_sw)
+                        self.optical_thickness_due_to_aerosol_sw=get_waveband_data_array(result[0].values.T, units='dimensionless', numlevels=self.numlevels, sw=True).T
+                        self.single_scattering_albedo_aerosol_sw = get_waveband_data_array(1, units='dimensionless', numlevels=self.numlevels, sw=True).T 
+                       
                         if not self.includeScattering:
                             raise ValueError('For aerosols scattering and absorption can not both be deactivated')
                     except (ValueError):
@@ -150,11 +229,17 @@ class VolcanoAerosol(Aerosol):
                     
 
     def calculateHeightLevels(self, atmosphere):
+        '''Used to translate the aerosol forcing data given as a function of height 
+        to aerosol forcing data as a function of pressure levels
+        '''
         heights = ty.pressure2height(atmosphere['plev'], atmosphere['T'][0, :])/1000
         return heights
 
 
 class NoAerosol(Aerosol):
+    '''
+    No volcanic aerosol
+    '''
     def __init__(self,atmNumlevels):
         super().__init__(atmNumlevels,aerosol_type='no_aerosol')
         
